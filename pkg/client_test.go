ec547184c265dcc820c6c5885d5a4e2ade578353
huaineng.zhang <huaineng.zhang@shanbay.com>
2023-12-22 22:39:36 +0800

    Refactor & connection pool

---
+++ pkg/client_test.go
@@ -0,0 +1,70 @@
+package pkg
+
+import (
+	"context"
+	"io"
+	"testing"
+	"time"
+
+	gomock "github.com/golang/mock/gomock"
+	"github.com/stretchr/testify/assert"
+)
+
+var mkCon *MockConnection
+var mkProtocol *MockProtocol
+var testClient Client
+
+func initTestClient(ctr *gomock.Controller) {
+	mkCon = NewMockConnection(ctr)
+	mkProtocol = NewMockProtocol(ctr)
+	newCon := func(_ string) Connection {
+		return mkCon
+	}
+	newProtocol := func(_ io.ReadWriter) Protocol {
+		return mkProtocol
+	}
+
+	testClient = &client{address: "1.1.1.1:6379", newConnection: newCon,
+		newProtocol: newProtocol,
+	}
+}
+
+func TestGet(t *testing.T) {
+	ctr := gomock.NewController(t)
+	defer ctr.Finish()
+	initTestClient(ctr)
+
+	key := []byte("key")
+	res := []byte("value")
+	mkCon.EXPECT().Connect().Return(nil).Times(1)
+	mkCon.EXPECT().Close().Return(nil).Times(1)
+	mkProtocol.EXPECT().WriteBulkStringArray([][]byte{
+		[]byte("GET"), key}).Return(nil).Times(1)
+	mkProtocol.EXPECT().ReadBulkString().Return(&res, nil).Times(1)
+
+	val, err := testClient.Get(context.TODO(), string(key))
+	assert.Nil(t, err)
+	assert.Equal(t, res, *val)
+}
+
+func TestSetWhileReturnOk(t *testing.T) {
+	ctr := gomock.NewController(t)
+	defer ctr.Finish()
+	initTestClient(ctr)
+
+	key := []byte("key")
+	val := []byte("value")
+
+	mkCon.EXPECT().Connect().Return(nil).Times(1)
+	mkCon.EXPECT().Close().Return(nil).Times(1)
+
+	mkProtocol.EXPECT().WriteBulkStringArray([][]byte{[]byte("SET"), key, val}).Return(nil).Times(1)
+	mkProtocol.EXPECT().GetNextMsgType().Return(MsgType(SimpleStringType), nil).Times(1)
+	mkProtocol.EXPECT().ReadSimpleString().Return([]byte("OK"), nil).Times(1)
+
+	ctx, _ := context.WithTimeout(context.Background(), 3*time.Second)
+	ret, err := testClient.Set(ctx, string(key), string(val))
+	assert.Nil(t, err)
+	assert.True(t, ret)
+
+}
